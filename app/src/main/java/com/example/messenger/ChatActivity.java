4a8fbaba4d9d78331dfa320d50161f5c7d7eaf7e
AlexandrProdan <prodan.alexandru.simion@gmail.com>
2023-01-03 18:35:48 +0100

    Sending and receiving functional implemented.

--- app/src/main/java/com/example/messenger/ChatActivity.java
+++ app/src/main/java/com/example/messenger/ChatActivity.java
@@ -1,22 +1,28 @@
 package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ChatActivity extends AppCompatActivity {
-    public static final String EXTRA_CURRENT_USER_ID = "currentUserID";
-    public static final String EXTRA_OTHER_USER_ID = "otherUserID";
+    private static final String TAG = "ChatActivity";
+    private static final String EXTRA_CURRENT_USER_ID = "currentUserID";
+    private static final String EXTRA_OTHER_USER_ID = "otherUserID";
     private String currentUserId;
     private String otherUserId;
 
@@ -27,6 +33,8 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView recyclerViewMessages;
     private MessagesAdapter messagesAdapter;
 
+    ChatVM chatVM;
+    ChatVMFactory chatVMFactory;
 
 
     @Override
@@ -37,29 +45,23 @@ public class ChatActivity extends AppCompatActivity {
         currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
         otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
 
+        chatVMFactory = new ChatVMFactory(currentUserId, otherUserId);
+        chatVM = new ViewModelProvider(this, chatVMFactory).get(ChatVM.class);
+
         messagesAdapter = new MessagesAdapter(currentUserId);
         recyclerViewMessages.setAdapter(messagesAdapter);
-//--------------Tests--------------
-        List<Message> messageList = new ArrayList<>();
-        for (int i=0; i<10;i++){
-            Message message = new Message(
-                    "Text "+i,
-                    currentUserId,
-                    otherUserId
-            );
-            messageList.add(message);
-        }
 
-        for (int i=0; i<10;i++){
-            Message message = new Message(
-                    "Text "+i,
-                    otherUserId,
-                    currentUserId
-            );
-            messageList.add(message);
-        }
+        observeViewModel();
 
-        messagesAdapter.setMessageList(messageList);
+        imageViewSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String messageText = editTextInputMessage.getText().toString().trim();
+                Message message = new Message(messageText, currentUserId, otherUserId);
+                chatVM.sendMessage(message);
+            }
+        });
+
     }
 
     void initViews(){
@@ -68,6 +70,43 @@ public class ChatActivity extends AppCompatActivity {
         editTextInputMessage = findViewById(R.id.editTextInputMessage);
         imageViewSend = findViewById(R.id.imageViewSend);
         recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+    }
+
+    void observeViewModel(){
+        chatVM.getMessagesListLD().observe(this, new Observer<List<Message>>() {
+            @Override
+            public void onChanged(List<Message> messages) {
+                messagesAdapter.setMessageList(messages);
+            }
+        });
+
+        chatVM.getErrorLD().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMsg) {
+                if(errorMsg != null){
+                    Toast.makeText(ChatActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Error "+errorMsg);
+                }
+            }
+        });
+
+        chatVM.getMessageSentLD().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean messageSent) {
+                if(messageSent){
+                    editTextInputMessage.setText("");
+                }
+            }
+        });
+
+        chatVM.getOtherUserLD().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                String userNameLastname = ""+user.getName()+" "+user.getLastName();
+                textViewTitle.setText(userNameLastname);
+            }
+        });
+
     }
 
     public static Intent newIntent(Context context, String currentUserId, String otherUserId){
---
+++ app/src/main/java/com/example/messenger/ChatVM.java
@@ -0,0 +1,121 @@
+package com.example.messenger;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatVM extends ViewModel {
+    private MutableLiveData<List<Message>> messagesListLD = new MutableLiveData<>();
+    private MutableLiveData<User> otherUserLD = new MutableLiveData<>();
+    private MutableLiveData<Boolean> messageSentLD = new MutableLiveData<>();
+    private MutableLiveData<String> errorLD = new MutableLiveData<>();
+
+    private String currentUserId;
+    private String otherUserId;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference refUsers = database.getReference("Users");
+    private DatabaseReference refMessages = database.getReference("Messages");
+    //---------------------------------Constructor--------------------------------------------------
+    public ChatVM(String currentUserId, String otherUserId) {
+        this.currentUserId = currentUserId;
+        this.otherUserId = otherUserId;
+
+        refUsers.child(otherUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User otherUserFromDb = snapshot.getValue(User.class);
+                otherUserLD.setValue(otherUserFromDb);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        refMessages.child(currentUserId).child(otherUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<Message> messageListFromDb = new ArrayList<>();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Message messageFromDB = dataSnapshot.getValue(Message.class);
+                    messageListFromDb.add(messageFromDB);
+                }
+                messagesListLD.setValue(messageListFromDb);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    //----------------------------------getters----------------------------------------------------
+    public LiveData<List<Message>> getMessagesListLD() {
+        return messagesListLD;
+    }
+
+    public LiveData<User> getOtherUserLD() {
+        return otherUserLD;
+    }
+
+    public LiveData<Boolean> getMessageSentLD() {
+        return messageSentLD;
+    }
+
+    public LiveData<String> getErrorLD() {
+        return errorLD;
+    }
+
+    //-------------------------------sendMessage-------------------------------------------------
+    public void sendMessage(Message message){
+        refMessages.child(message.getSenderId())
+                .child(message.getReceiverId())
+                .push()
+                .setValue(message)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        refMessages.child(message.getReceiverId())
+                                .child(message.getSenderId())
+                                .push()
+                                .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        messageSentLD.setValue(true);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        errorLD.setValue(e.getMessage());
+                                    }
+                                });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        errorLD.setValue(e.getMessage());
+                    }
+                });
+    }
+
+
+}
